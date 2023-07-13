14cd55ebd53342b584272618c2b26c3fcaa724b9
Childofrainydays <107083153+Childofrainydays@users.noreply.github.com>
2023-07-13 00:24:51 -0600

    started array for questions in JS file

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+function generateQuiz() {
+    // Create an array of questions.
+    var questions = [
+      {
+        question: "What method is used to add an element to the end of an array in JavaScript?",
+        answers: ["push()","add()", "concat()", "append()"]
+      },
+      {
+        question: "Which operator is used for strict equality comparison in JavaScript?",
+        answers: ["==", "===", "=", "!="]
+      }
+    ];
+    // Rest of your code here...
+  }
