931920510afe515d9ebf4400c49dbf71d716ec1b
Childofrainydays <107083153+Childofrainydays@users.noreply.github.com>
2023-07-13 03:07:38 -0600

    CSS styling for homepage

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# guess-machine
+# Guess-machine
 Shall we play a game?
+
+This is a code quiz project to test your knowledge of coding concepts. The quiz contains multiple-choice questions: select the correct answer for each question.
+
+## Getting Started
+
+To get started with the code quiz project, follow these steps:
+
+1. Clone the repository or download the project files
+
+OR
+
+2. Try it out at the deployed page link here on GitHub :
+
+## Project Structure
+
+The project files are organized as follows:
+
+- `index.html`: HTML file containing the homepage for the quiz website - links to:
+- `quiz.html`: 
+- `complete.html`:
+- `styles.css`: CSS file for styling the quiz webpage.
+- `script.js`: JavaScript file containing the quiz logic and the `questions` array.
+
+## Contributing
+
+Contributions to this code quiz project are welcome! If you have any suggestions, bug reports, or feature requests, please open an issue or submit a pull request.
+
+## License
+
+This code quiz project is licensed under the [MIT License].
--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,38 @@
+
+
 function generateQuiz() {
     // Create an array of questions.
-    var questions = [
+    let questions = [
       {
         question: "What method is used to add an element to the end of an array in JavaScript?",
-        answers: ["push()","add()", "concat()", "append()"]
+        answers: ["push()", "add()", "concat()", "append()"],
+        correctAnswer: 0 // Index of the correct answer
       },
       {
         question: "Which operator is used for strict equality comparison in JavaScript?",
-        answers: ["==", "===", "=", "!="]
+        answers: ["==", "===", "=", "!="],
+        correctAnswer: 1 // Index of the correct answer
+      },
+      {
+        question: "What is the purpose of the getElementById method in JavaScript?",
+        answers: ["To select an element by its class name","To select the first element that matches a specified CSS selector", "To select an element by its ID", "To select all elements that match a specified CSS selector"],
+        correctAnswer: 2 // Index of the correct answer
+      },
+      {
+        question: "Which of the following is NOT a JavaScript data type?",
+        answers: ["string","boolean", "integer", "object"],
+        correctAnswer: 2 // Index of the correct answer
+      },
+      {
+        question: "Which of the following is the correct way to create a function in JavaScript??",
+        answers: ["function myFunction() { }","myFunction() { }", "create myFunction() { }", "new Function() { }"],
+        correctAnswer: 0 // Index of the correct answer
+      },
+      {
+        question: "Which of the following is not a valid JavaScript variable name?",
+        answers: ["myVariable", "_myVariable", "$variable", "None of the above"],
+        correctAnswer: 3 // Index of the correct answer
       }
     ];
-    // Rest of your code here...
+    // Rest of code here...
   }
